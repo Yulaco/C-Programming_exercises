0e161982ee921b46e0033921811509eb045b3cc7
Yulaco <cleochen93@gmail.com>
2023-11-02 00:35:49 -0700

    Program checks equality of two numbers

---
+++ 0x00-conditional_statement/0-check_equality.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	int num1 = 15;
+	int num2 = 15;
+
+	if (num1 == num2)
+		printf("The two numbers are equal\n");
+
+	else
+		printf("The two numbers are not equal\n");
+
+	return (0);
+}
