b60c22f062dfdacf3f521e68fa8e748e77bb53e5
Yulaco <cleochen93@gmail.com>
2023-11-21 11:12:00 -0800

    Program reads value and displays a number

---
+++ 0x00-conditional_statement/5-read_value.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	int m = -5;
+
+	if (m > 0)
+		printf("The value of n = 1\n");
+	if (m == 0)
+		printf("The value of n = 0\n");
+	else
+		printf("The value of n = -1\n");
+	return (0);
+}
